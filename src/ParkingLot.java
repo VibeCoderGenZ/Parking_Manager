b07f6b35be070e077b6b7c089295cd866fe20bb3
Mai Huu Son <maihuuson2005@gmail.com>
2025-11-06 08:14:19 +0700

    hoàn thành parkinglot thiếu login

--- src/ParkingLot.java
+++ src/ParkingLot.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLot {
+    private final List<ParkingZone> parkingZones;
+
+
+    public ParkingLot() {
+        this.parkingZones = new ArrayList<>();
+    }
+
+    public List<ParkingZone> getParkingZones() {
+        return parkingZones;
+    }
+
+    public void addZone(ParkingZone zone) {
+        this.parkingZones.add(zone);
+    }
 }
